c755fb483e1c35042730d5ee485b2b9736c135bf
mrguilher621 <mrguilher621@gmail.com>
2017-07-04 16:45:35 -0300

    1 fase completa

--- Interdiciplinar1.1/App_Start/RouteConfig.cs
+++ Interdiciplinar1.1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Interdiciplinar1._1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Categorias", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Interdiciplinar1.1/Controllers/CategoriasController.cs
@@ -0,0 +1,76 @@
+﻿using Interdiciplinar1._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interdiciplinar1._1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria(){CategoriaId=1,Nome="Celulares"}
+        };
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+
+        // GET: Categorias/Details/5
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        // GET: Categorias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorias/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(c => c.CategoriaId).Max() + 1;
+           
+                return RedirectToAction("Index");
+          
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        // POST: Categorias/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+           
+                return RedirectToAction("Index");
+            
+        }
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(c => c.CategoriaId == id).First());
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
